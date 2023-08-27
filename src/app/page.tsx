ec3496e316200a44dee5af3e7cbf31e30d6cae87
Sandra S <zandrastr@hotmail.com>
2023-08-27 18:01:06 +0200

    add onchange event to form and create usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
+
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+
   return (
     <>
       <div>
         <h1>Person generator</h1>
         <form>
-          <input type='text' placeholder='Type a name' />
+          <input type='text' placeholder='Type a name' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           <button type='submit'>Get data</button>
         </form>
       </div>
