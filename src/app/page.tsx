08e793aeb4149c074a36c2773ee53d0aafb53a11
Sandra S <zandrastr@hotmail.com>
2023-08-27 17:52:31 +0200

    add name input field and submit button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Person generator</h1>
+        <form>
+          <input type='text' placeholder='Type a name' />
+          <button type='submit'>Get data</button>
+        </form>
+      </div>
+    </>
+  );
 }
