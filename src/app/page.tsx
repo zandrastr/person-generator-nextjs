ce113e1cc0f498d22c9094c4536ef482d1c682b0
Sandra S <zandrastr@hotmail.com>
2023-08-27 18:04:25 +0200

    add onsubmit event to form to prevent default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 'use client';
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <>
       <div>
         <h1>Person generator</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type='text' placeholder='Type a name' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
           <button type='submit'>Get data</button>
         </form>
