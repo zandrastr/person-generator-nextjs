bc56a6d4a8dced2f031e5717175598bd7abeba63
Sandra S <zandrastr@hotmail.com>
2023-09-01 20:47:35 +0200

    add styling to main form element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,16 @@ export default function Home() {
 
   return (
     <>
-      <div>
-        <h1>Person generator</h1>
-        <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='Type a name' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-          <button type='submit'>Get data</button>
-        </form>
+      <div className='h-full flex flex-col items-center justify-center'>
+        <div className='border-2 p-8 rounded-lg'>
+          <h1 className='text-4xl font-bold tracking-wide uppercase mb-5'>Person generator</h1>
+          <form className='flex flex-col' onSubmit={handleSubmit}>
+            <input className='p-2 rounded-lg text-black mb-4' type='text' placeholder='Type a name' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <button className='p-2 rounded-lg bg-blue-900 hover:bg-blue-600 font-bold' type='submit'>
+              Get data
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
